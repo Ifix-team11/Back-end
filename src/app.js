b9c59bae5c4ef55672b20e8b1e12fa29edff1caa
hager1nady <hajrnady24@gmail.com>
2026-08-05 17:43:58 +0300

    fix swagger routing on vercel

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
+console.log("Swagger loaded");
 
 // Auth Routes
 app.use("/api/auth", authRoutes);
